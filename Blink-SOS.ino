351c728760f9a1d975c2b680b3c36d587cf69ca7
adamhedin <adam.s.hedin@gmail.com>
2024-05-31 13:42:36 +0200

    init

---
+++ Blink-SOS.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);   
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);   
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+}
